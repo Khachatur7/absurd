1da78653d4591df04aab0af644c6ac8fcc31bb3c
hacikhacatran7@gmail.com <hacikhacatran7@gmail.com>
2025-02-18 21:32:51 +0400

    first commit

--- src/pages/Loading/Loading.tsx
+++ src/pages/Loading/Loading.tsx
@@ -1,12 +1,21 @@
 import { useNavigate } from "react-router-dom";
 import style from "./Loading.module.css";
+import { useEffect } from "react";
 
 const Loading = () => {
   const navigate = useNavigate();
   const storage = localStorage.getItem("first-time");
   const firstVisiting = storage ? JSON.parse(storage) : false;
-  console.log(firstVisiting);
   const nextPage = firstVisiting ? "/absurd" : "/prequel";
+
+  useEffect(() => {
+    const navigateTimer = setTimeout(() => {
+      navigate(nextPage);
+    }, 4000);
+    return () => {
+      clearTimeout(navigateTimer);
+    };
+  }, []);
 
   return (
     <div className={style["loading-page"]}>
